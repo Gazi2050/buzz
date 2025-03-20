604985458e3486570f5eac472992a8c25f7fcc96
Gazi2050 <gazi45344@gmail.com>
2025-03-20 21:13:16 +0600

    feat:generateUsername and generatePassword function added

---
+++ buzz-frontend/src/utils/generateUsername.ts
@@ -0,0 +1,23 @@
+const apiUrl = import.meta.env.VITE_USERNAME_GENERATE_API_URL;
+const apiKey = import.meta.env.VITE_USERNAME_GENERATE_API_KEY;
+
+export async function generateUsername(): Promise<string> {
+    try {
+        const response = await fetch(apiUrl, {
+            method: "GET",
+            headers: {
+                "X-Api-Key": apiKey,
+            },
+        });
+        if (!response.ok) {
+            throw new Error(`Error: ${response.status} ${response.statusText}`);
+        }
+        const data = await response.json();
+        const randomNum = Math.floor(Math.random() * 990) + 10; // 10 to 999
+        return `${data.username}_${randomNum}`;
+    } catch (error) {
+        console.error("Failed to generate username:", error);
+        const randomNum = Math.floor(Math.random() * 990) + 10; // 10 to 999
+        return `user_${randomNum}`;
+    }
+}
