ee4395094edbf27399711988100073909e9cbd23
Gazi2050 <gazi45344@gmail.com>
2025-03-30 17:14:50 +0600

    improve : header and navbar

--- buzz-frontend/src/components/Header.svelte
+++ buzz-frontend/src/components/Header.svelte
@@ -5,10 +5,10 @@
 </script>
 
 <div
-    class="fixed top-0 right-0 left-0 flex justify-center items-center z-50 mt-3"
+    class="fixed top-0 right-0 left-0 flex justify-center items-center z-50 mt-3 bg-transparent backdrop-blur-lg max-w-[1230px] w-full mx-auto rounded-xl"
 >
     <div
-        class="max-w-[1230px] w-full flex justify-between items-center border border-gray-800 px-3 py-3 rounded-xl mx-1 bg-gray-800/30"
+        class="max-w-[1230px] w-full flex justify-between items-center border border-gray-800 px-3 py-3 rounded-xl mx-1 lg:mx-0 bg-gray-800/30"
     >
         <a href="/" class="text-4xl font-bold text-[#6AFF33] -mt-[9px]">
             Buzz
@@ -18,7 +18,7 @@
                 href="/profile"
                 class="flex justify-center items-center rounded-full p-2 bg-[#6AFF33]"
             >
-                <User class="w-7 h-7 md:w-9 md:h-9 text-black" />
+                <User size={28} class="text-black" />
             </a>
         {:else}
             <a href="/sign-in"
--- buzz-frontend/src/components/Navbar.svelte
+++ buzz-frontend/src/components/Navbar.svelte
@@ -4,39 +4,41 @@
 </script>
 
 <div
-    class="fixed bottom-10 sm:bottom-16 md:bottom-20 left-1/2 transform -translate-x-1/2 w-full max-w-[350px] md:max-w-[500px] p-4 bg-transparent backdrop-blur-lg rounded-xl shadow-lg text-white z-50"
+    class="w-full max-w-[350px] md:max-w-[500px] fixed bottom-10 sm:bottom-16 md:bottom-20 left-1/2 transform -translate-x-1/2 bg-transparent backdrop-blur-lg rounded-xl"
 >
-    <div class="flex justify-evenly items-center gap-3 sm:gap-5">
-        <a
-            href="/"
-            class="transition-transform transform hover:scale-125 duration-300 p-[6px] hover:bg-white hover:text-black rounded-full"
-        >
-            <House />
-        </a>
-        <a
-            href="/my-post"
-            class="transition-transform transform hover:scale-125 duration-300 p-[6px] hover:bg-white hover:text-black rounded-full"
-        >
-            <Layers />
-        </a>
-        <a
-            href="/create-post"
-            class="transition-transform transform hover:scale-125 duration-300"
-        >
-            <Plus size={35} class="bg-white text-black rounded-full" />
-        </a>
-        <a
-            href="/"
-            class="transition-transform transform hover:scale-125 duration-300 p-[6px] hover:bg-white hover:text-black rounded-full"
-        >
-            <Bell />
-        </a>
-        <button
-            onclick={signout}
-            class="transition-transform transform hover:scale-125 duration-300 p-[6px] hover:bg-white hover:text-black rounded-full"
-        >
-            <!-- <Sun /> -->
-            <LogOut />
-        </button>
+    <div class=" p-4 bg-gray-800/30 rounded-xl shadow-lg text-white z-50">
+        <div class="flex justify-evenly items-center gap-3 sm:gap-5">
+            <a
+                href="/"
+                class="transition-transform transform hover:scale-125 duration-300 p-[6px] hover:bg-white hover:text-black rounded-full"
+            >
+                <House />
+            </a>
+            <a
+                href="/my-post"
+                class="transition-transform transform hover:scale-125 duration-300 p-[6px] hover:bg-white hover:text-black rounded-full"
+            >
+                <Layers />
+            </a>
+            <a
+                href="/create-post"
+                class="transition-transform transform hover:scale-125 duration-300"
+            >
+                <Plus size={35} class="bg-white text-black rounded-full" />
+            </a>
+            <a
+                href="/"
+                class="transition-transform transform hover:scale-125 duration-300 p-[6px] hover:bg-white hover:text-black rounded-full"
+            >
+                <Bell />
+            </a>
+            <button
+                onclick={signout}
+                class="transition-transform transform hover:scale-125 duration-300 p-[6px] hover:bg-white hover:text-black rounded-full"
+            >
+                <!-- <Sun /> -->
+                <LogOut />
+            </button>
+        </div>
     </div>
 </div>
